83183e2c1deae005c3470b18b887181b68f384df
Allison <farrella95@gmail.com>
2024-06-02 18:56:38 -0400

    js file connected

---
+++ app.js
@@ -0,0 +1,9 @@
+let subscribe = document.querySelector(".subscribe");
+let emailInput = document.querySelector("#email-name");
+
+subscribe.addEventListener("click", () => {
+  console.log("ayeeeee");
+});
+emailInput.addEventListener("click", () => {
+  console.log("ayeeeee");
+});
